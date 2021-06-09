a381c72203c6f46259e21769e20e7764c8571acf
Avinash <Avinash@DESKTOP-IT01653.mshome.net>
2021-06-09 11:19:10 +0530

    diagnostic update

---
+++ Hackathon_Project/Hackathon_FindingHospitals/src/main/java/pObjects/DiagnosticPageLocators.java
@@ -0,0 +1,30 @@
+package pObjects;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class DiagnosticPageLocators {
+	public static WebElement element;
+	public static WebDriver driver;
+	
+	public static WebElement searchCity(WebDriver driver)
+	{
+		element= driver.findElement(By.xpath("//input[@id='locationInput']"));
+		return element;
+	}
+	public static WebElement diagnostics(WebDriver driver)
+	{
+		element= driver.findElement(By.xpath("//div[@class='product-tab__title'][text()='Diagnostics']"));
+		return element;
+	}
+	public static List<WebElement> topCities(WebDriver driver)
+	{
+		System.out.println("hello");
+		List<WebElement> cities= driver.findElements(By.xpath("//ul/li/div[2]"));
+		return cities;
+	}
+        
+}
